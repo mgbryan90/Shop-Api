3b4e7ce498450305f5c832824b7a867bf232d05e
Bryan <mgbryan90@hotmail.com>
2022-11-10 03:19:14 -0500

    Add filter controller

--- src/controllers/filter.controller.js
+++ src/controllers/filter.controller.js
@@ -3,14 +3,15 @@ import { getConnection } from "./../database/database";
 // Para listar Productos
 // Status 500 para ver si existe error del servidor en la consulta
 const getFilter = async (request, response) => {
+  const { mode } = request.body;
   try {
     const connection = await getConnection();
     const result = await connection.query(
-      `SELECT * FROM product WHERE name = '${product.name}'`
+      `SELECT * FROM product WHERE name LIKE '%${mode}%'`
     );
     response.json(result);
   } catch (error) {
-    response.status(500);
+    response.status(error?.status || 500);
     response.send(error.message);
   }
 };
